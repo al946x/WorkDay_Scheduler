38ce3b984b68b703cffaccd98e67ebd6742126ef
Alina Ali01~ <alina.zenam@hotmail.com>
2023-01-08 18:09:11 +0000

    timeblocks created properly and funtion update schedule created

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ currentDay = $('#currentDay');
 $(currentDay).text(moment().format('MMM Do YYYY'));
 
 
+//show timeblocks for every hour from 9-5
+var container = $('.container');
+
 var timeblocks = {
-    9: '',
-    10: '',
-    11: '',
-    12: '',
-    13: '',
-    14: '',
-    15: '',
-    16: '',
-    17: ''
+   9: '',
+   10: '',
+   11: '',
+   12: '',
+   13: '',
+   14: '',
+   15: '',
+   16: '',
+   17: ''
+}
+
+function updateSchedule(){
+    $.each(timeblocks, function(key, value){
+       var hour = key;
+       var tasks = value;
+       container.append(`
+       <section class="row" id="hourSlot">
+       <div class="timeblock hour">${hour}</div>
+       <span> hs</span>
+       <textarea name="tasks" id="tasks" rows="5" cols="80">${value}</textarea>
+       <button class="saveBtn">Save</button>
+       </section>
+       `);
+    })
  }
+ 
